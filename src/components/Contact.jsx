1822d3fb65d5844f4d4be1338881ddbebc004f6e
moeti <moetichentuf@hotmail.com>
2020-07-09 13:38:26 +0200

    finished

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,7 @@ function Contact() {
           </div>
           <div class="col-lg-5">
             <h1 class="font-weight-light mb-5">Contact</h1>
-            <h1  style={{marginLeft:'5%'}}><a href="moetichentuf@hotmail.com"> <span className="glyphicon">&#x2709;</span>
+            <h1  style={{marginLeft:'5%'}}><a href="mailto: moetichentuf@hotmail.com"> <span className="glyphicon">&#x2709;</span>
             </a></h1>
             <h1><a href="https://github.com/moetichentuf" className="fa fa-github"></a></h1>
             <h1><a href="https://www.linkedin.com/in/moeti-chentuf-1bb088198/" className="fa fa-linkedin"></a></h1>
