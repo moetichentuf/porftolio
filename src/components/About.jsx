d27c882f23e5e46fc53eab981a31f297ebe65636
moeti <moetichentuf@hotmail.com>
2020-07-09 10:53:41 +0200

    added cv

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import moeti from "./images/MoetiCV.jpg"
+import Gallery from 'react-grid-gallery';
 function About() {
+  const CV =
+      [{
+        src: moeti,
+        thumbnail: moeti,
+        thumbnailWidth: 100,
+        thumbnailHeight: 'auto',
+
+      }]
   return (
     <div className="about">
       <div class="container">
+          <Gallery  images={CV}/>
         <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img style={{width:"60%"}}
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src={moeti}
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
+
+
+          <div class="col-lg-9
+          ml-5">
+
             <h1 class="font-weight-light">CV</h1>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -20,6 +27,7 @@ function About() {
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
+
           </div>
         </div>
       </div>
